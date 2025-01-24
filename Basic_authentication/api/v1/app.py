aac69d54404ae3c6866cf28ec1508f13d62e5a7b
bvilane <b.vilane@alustudent.com>
2025-01-24 21:40:19 +0200

    new update

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,6 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv('AUTH_TYPE') == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif getenv('AUTH_TYPE') == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -19,6 +27,37 @@ def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def access_denied(error) -> str:
+    """
+    Uh Uh Uh. You didnt say the magic word
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def access_forbidden(error) -> str:
+    """
+    No cookies for you
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request_handler():
+    """before_request handler"""
+    excluded = ['/api/v1/status/',
+                '/api/v1/unauthorized/',
+                '/api/v1/forbidden/']
+
+    if auth is not None and auth.require_auth(request.path, excluded):
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """
+    GET /api/v1/unauthorized
+    return unauthorized message
+    """
+    abort(401, description="UNAUTHORIZED")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """
+    GET /api/v1/forbidden
+    return 403 page
+    """
+    abort(403, description="FORBIDDEN")
